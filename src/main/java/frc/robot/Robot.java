26c74d7f0a87665b27d162a91655b472ace82f12
ClaytonM223 <60757438+ClaytonM223@users.noreply.github.com>
2022-03-03 09:29:46 -0500

    test

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,6 +12,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 // proof that this works
+// relocated files
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
