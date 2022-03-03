2797b0629635a9cdaf20100c2c1c50650ed8613e
ClaytonM223 <cjaymosier@gmail.com>
2022-03-03 09:24:21 -0500

    double check

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -11,7 +11,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
-//testing the other direction
+// proof that this works
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
