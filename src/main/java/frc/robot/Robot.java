9ce8f3c7b510d93fb1bf690f5bcc96897b77b843
ClaytonM223 <60757438+ClaytonM223@users.noreply.github.com>
2022-03-03 09:15:10 -0500

    testing

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -11,9 +11,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
-// testing changes
-// testing continues
-// did this work?
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
