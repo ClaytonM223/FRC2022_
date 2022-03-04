091ccb908250bf03398f5fcc047c3eecc484f6f7
ClaytonM223 <60757438+ClaytonM223@users.noreply.github.com>
2022-03-04 09:52:29 -0500

    added notes

--- src/main/java/frc/robot/commands/Notes.java
+++ src/main/java/frc/robot/commands/Notes.java
@@ -1,6 +1,7 @@
 /*
 Shooter error:
 [CAN SPARK MAX] IDs: 7, timed out while waiting for Periodic Status 1
+Solution: motor drawing too much current so enabled current limit to 35 amps and added a ramp rate
 
 
 
