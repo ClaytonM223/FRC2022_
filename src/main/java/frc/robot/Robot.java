f8a1aaa5185d66fd5d3871b607393150dd388211
ClaytonM223 <60757438+ClaytonM223@users.noreply.github.com>
2022-03-07 09:21:13 -0500

    commit

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -11,6 +11,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.Constants.AutoNumbers;
 import frc.robot.subsystems.Shooter;
 
 
@@ -82,7 +83,10 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during autonomous. */
   @Override
-  public void autonomousPeriodic() {}
+  public void autonomousPeriodic() {
+    RobotContainer.shooter.shooter.set(AutoNumbers.ShooterPower);
+    
+  }
 
   @Override
   public void teleopInit() {
