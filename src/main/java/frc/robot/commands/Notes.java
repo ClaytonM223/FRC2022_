6a7e2273dc85873910801b4b3bb2119523a3d8dd
ClaytonM223 <60757438+ClaytonM223@users.noreply.github.com>
2022-03-11 10:23:36 -0500

    Added to notes

--- src/main/java/frc/robot/commands/Notes.java
+++ src/main/java/frc/robot/commands/Notes.java
@@ -3,6 +3,110 @@ Shooter error:
 [CAN SPARK MAX] IDs: 7, timed out while waiting for Periodic Status 1
 Solution: motor drawing too much current so enabled current limit to 35 amps and added a ramp rate
 
+[[[TABLE OF LED VALUES]]]
+
+Value              Type                  Style                                  Color/Pattern
+
+-0.99       Fixed Palette Pattern       Rainbow,                            Rainbow Palette Pattern
+-0.97       Fixed Palette Pattern       Rainbow,                            Party Palette Pattern 
+-0.95       Fixed Palette Pattern       Rainbow,                            Ocean Palette Pattern 
+-0.93       Fixed Palette Pattern       Rainbow,                            Lave Palette Pattern 
+-0.91       Fixed Palette Pattern       Rainbow,                            Forest Palette Pattern 
+-0.89       Fixed Palette Pattern                                           Rainbow with Glitter Pattern 
+-0.87       Fixed Palette Pattern                                           Confetti Pattern 
+-0.85       Fixed Palette Pattern       Shot,                               Red 
+-0.83       Fixed Palette Pattern       Shot,                               Blue 
+-0.81       Fixed Palette Pattern       Shot,                               White 
+-0.79       Fixed Palette Pattern       Sinelon,                            Rainbow Palette Pattern 
+-0.77       Fixed Palette Pattern       Sinelon,                            Party Palette Pattern 
+-0.75       Fixed Palette Pattern       Sinelon,                            Ocean Palette Pattern 
+-0.73       Fixed Palette Pattern       Sinelon,                            Lava Palette Pattern 
+-0.71       Fixed Palette Pattern       Sinelon,                            Forest Palette Pattern 
+-0.69       Fixed Palette Pattern       Beats per Minute,                   Rainbow Palette Pattern 
+-0.67       Fixed Palette Pattern       Beats per Minute,                   Party Palette Pattern 
+-0.65       Fixed Palette Pattern       Beats per Minute,                   Ocean Palette Pattern 
+-0.63       Fixed Palette Pattern       Beats per Minute,                   Lava Palette Pattern 
+-0.61       Fixed Palette Pattern       Beats per Minute,                   Forest Palette Pattern 
+-0.59       Fixed Palette Pattern       Fire,                               Medium 
+-0.57       Fixed Palette Pattern       Fire,                               Large 
+-0.55       Fixed Palette Pattern       Twinkles,                           Rainbow Palette 
+-0.53       Fixed Palette Pattern       Twinkles,                           Party Palette 
+-0.51       Fixed Palette Pattern       Twinkles,                           Ocean Palette 
+-0.49       Fixed Palette Pattern       Twinkles,                           Lava Palette 
+-0.47       Fixed Palette Pattern       Twinkles,                           Forest Palette 
+-0.45       Fixed Palette Pattern       Color Waves,                        Rainbow Palette 
+-0.43       Fixed Palette Pattern       Color Waves,                        Party Palette 
+-0.41       Fixed Palette Pattern       Color Waves,                        Ocean Palette 
+-0.39       Fixed Palette Pattern       Color Waves,                        Lava Palette 
+-0.37       Fixed Palette Pattern       Color Waves,                        Forest Palette 
+-0.35       Fixed Palette Pattern       Larson Scanner,                     Red Pattern 
+-0.33       Fixed Palette Pattern       Larson Scanner,                     Gray Pattern 
+-0.31       Fixed Palette Pattern       Light Chase,                        Red 
+-0.29       Fixed Palette Pattern       Light Chase,                        Blue 
+-0.27       Fixed Palette Pattern       Light Chase,                        Gray 
+-0.25       Fixed Palette Pattern       Heartbeat,                          Red 
+-0.23       Fixed Palette Pattern       Heartbeat,                          Blue 
+-0.21       Fixed Palette Pattern       Heartbeat,                          White 
+-0.19       Fixed Palette Pattern       Heartbeat,                          Gray 
+-0.17       Fixed Palette Pattern       Breath,                             Red 
+-0.15       Fixed Palette Pattern       Breath,                             Blue 
+-0.13       Fixed Palette Pattern       Breath,                             Gray 
+-0.11       Fixed Palette Pattern       Strobe,                             Red 
+-0.09       Fixed Palette Pattern       Strobe,                             Blue 
+-0.07       Fixed Palette Pattern       Strobe,                             Gold 
+-0.05       Fixed Palette Pattern       Strobe,                             White 
+-0.03       Color 1 Pattern             End to End Blend to Black 
+-0.01       Color 1 Pattern             Larson Scanner Pattern 
+ 0.01       Color 1 Pattern             Light Chase 
+ 0.03       Color 1 Pattern             Heartbeat                           Slow 
+ 0.05       Color 1 Pattern             Heartbeat                           Medium 
+ 0.07       Color 1 Pattern             Heartbeat                           Fast 
+ 0.09       Color 1 Pattern             Breath                              Slow 
+ 0.11       Color 1 Pattern             Breath                              Fast 
+ 0.13       Color 1 Pattern                                                 Shot 
+ 0.15       Color 1 Pattern                                                 Strobe 
+ 0.17       Color 2 Pattern             End to End Blend to Black 
+ 0.19       Color 2 Pattern             Larson Scanner Pattern 
+ 0.21       Color 2 Pattern             Light Chase 
+ 0.23       Color 2 Pattern             Heartbeat                           Slow 
+ 0.25       Color 2 Pattern             Heartbeat                           Medium 
+ 0.27       Color 2 Pattern             Heartbeat                           Fast 
+ 0.29       Color 2 Pattern             Breath                              Slow 
+ 0.31       Color 2 Pattern             Breath                              Fast 
+ 0.33       Color 2 Pattern                                                 Shot 
+ 0.35       Color 2 Pattern                                                 Strobe 
+ 0.37       Color 1 and 2 Pattern       Sparkle,                            Color 1 on Color 2 
+ 0.39       Color 1 and 2 Pattern       Sparkle,                            Color 2 on Color 1 
+ 0.41       Color 1 and 2 Pattern       Color Gradient,                     Color 1 and 2 
+ 0.43       Color 1 and 2 Pattern       Beats per Minute,                   Color 1 and 2 Pattern 
+ 0.45       Color 1 and 2 Pattern       End to End Blend,                   Color 1 to 2 
+ 0.47       Color 1 and 2 Pattern                                           End to End Blend 
+ 0.49       Color 1 and 2 Pattern       Color 1 and Color 2 no blending
+ 0.51       Color 1 and 2 Pattern       Twinkles,                           Color 1 and 2 
+ 0.53       Color 1 and 2 Pattern       Color Waves,                        Color 1 and 2 
+ 0.55       Color 1 and 2 Pattern       Sinelon,                            Color 1 and 2 Pattern 
+ 0.57       Solid Colors                                                    Hot Pink 
+ 0.59       Solid Colors                                                    Dark red 
+ 0.61       Solid Colors                                                    Red 
+ 0.63       Solid Colors                                                    Red Orange 
+ 0.65       Solid Colors                                                    Orange 
+ 0.67       Solid Colors                                                    Gold 
+ 0.69       Solid Colors                                                    Yellow 
+ 0.71       Solid Colors                                                    Lawn Green 
+ 0.73       Solid Colors                                                    Lime 
+ 0.75       Solid Colors                                                    Dark Green 
+ 0.77       Solid Colors                                                    Green 
+ 0.79       Solid Colors                                                    Blue Green 
+ 0.81       Solid Colors                                                    Aqua 
+ 0.83       Solid Colors                                                    Sky Blue 
+ 0.85       Solid Colors                                                    Dark Blue 
+ 0.87       Solid Colors                                                    Blue 
+ 0.89       Solid Colors                                                    Blue Violet 
+ 0.91       Solid Colors                                                    Violet 
+ 0.93       Solid Colors                                                    White 
+ 0.95       Solid Colors                                                    Gray 
+ 0.97       Solid Colors                                                    Dark Gray 
+ 0.99       Solid Colors                                                    Black 
 
 
 */
